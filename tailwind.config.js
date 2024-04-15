953135d4333629182e8cb04dd33158714599da1a
Samarpita Das <144604345+SamarpitaBDas@users.noreply.github.com>
2024-04-15 15:45:53 +0530

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    theme: {
-        extend: {
-            backgroundImage: {
-                'gradient-custom': 'linear-gradient(to right, #7B91FF, #95BEFF)',
-            },
-        },
-    },
-};
